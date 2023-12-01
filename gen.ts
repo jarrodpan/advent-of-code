c2c5327204118219f337e0aabf77bf65ed6152b1
Jarrod Pan <jarrod.pan@gmail.com>
2023-12-01 17:24:45 +1100

    build: generator

---
+++ gen.ts
@@ -0,0 +1,27 @@
+import fs from 'fs';
+
+const [_node, _script, arg] = process.argv;
+
+if (!arg || !arg.match(/20[0-9]{2}\/[0-9]{2}/)) {
+  throw new Error(`Invalid format: ${arg}`);
+}
+
+const [year, day] = arg.split('/');
+
+try {
+  fs.mkdirSync(`./${year}`);
+} catch (e) {}
+try {
+  fs.mkdirSync(`./${arg}`);
+} catch (e) {}
+
+fs.writeFileSync(`./${arg}/input.txt`, '');
+fs.writeFileSync(`./${arg}/test.txt`, '');
+fs.writeFileSync(`./${arg}/test2.txt`, '');
+fs.writeFileSync(
+  `./${arg}/index.ts`,
+  `import fs from 'fs';
+
+console.log('Advent of Code ${arg}');
+const input = fs.readFileSync('./${arg}/input.txt', 'utf-8');`
+);
