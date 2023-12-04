a15a242fbc14fd10c9ddb2c66568d11d36175692
Jarrod Pan <jarrod.pan@gmail.com>
2023-12-04 14:14:05 +1100

    feat: fix generator regex

--- gen.ts
+++ gen.ts
@@ -31,7 +31,7 @@ let ans = 0;
 for (let i = 0; i < input.length; i++) {
   const line = input[i];
   
-  // const groups = line.match(/(?<aaaaa>\w+)(?<bbbbb>\d+)/)!.groups!;
+  // const groups = line.match(/(?<aaaaa>\\w+)(?<bbbbb>\\d+)/)!.groups!;
 }
 
 console.log({ ans });
