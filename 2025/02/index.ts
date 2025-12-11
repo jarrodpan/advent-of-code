bb0c9ebaeab2e29e77464e48de004886c20e44e9
Jarrod Pan <jarrod.pan@gmail.com>
2025-12-11 11:44:03 +1100

    feat: solved 2025/02/1

---
+++ 2025/02/index.ts
@@ -0,0 +1,65 @@
+import fs from 'fs';
+
+console.log('Advent of Code 2025/02');
+const input = fs.readFileSync('./2025/02/input.txt', 'utf-8').split('\n');
+
+let ans = 0;
+
+for (let i = 0; i < input.length; i++) {
+  const line = input[i];
+
+  const strRanges = line.split(',');
+  const splitRanges = strRanges.map((r) => {
+    return r.split('-');
+  });
+
+  const ranges = [];
+
+  for (const range of splitRanges) {
+    // exclude if odd length and same length for both ends
+    if (range[0].length === range[1].length && range[0].length % 2 === 1)
+      continue;
+
+    // bring odd length lower bound up to even
+    let lower = Number(range[0]);
+    while (lower.toString().length % 2 === 1) lower++;
+
+    // bring odd length upper bound down to even
+    let upper = Number(range[1]);
+    while (upper.toString().length % 2 === 1) upper--;
+
+    ranges.push([lower, upper]);
+
+    console.log(range, ranges.at(-1)!);
+  }
+
+  ranges.sort((a, b) => a[0] - b[0]);
+
+  console.table(ranges);
+
+  // start with 1 and iterate, if the tested number exceeds the upper bound of the start of the array then shift it
+  let test = 1;
+
+  while (ranges.length > 0) {
+    const id = test * 10 ** test.toString().length + test;
+    // pop ranges we have surpassed
+    while (ranges.length && ranges[0][1] < id) {
+      const excl = ranges.shift();
+      console.log('shifted range', excl);
+    }
+    if (ranges.length === 0) break;
+    const [lower, upper] = ranges[0];
+    if (lower <= id && id <= upper) {
+      ans += id;
+      console.log(lower, '<=', id, '<=', upper, '--->', ans);
+    }
+
+    test++;
+  }
+
+  // const groups = line.match(/(?<aaaaa>\w+)(?<bbbbb>\d+)/)!.groups!;
+}
+
+console.log({ ans });
+
+// test: 1227775554
