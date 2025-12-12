c4a0a9e504474384bedf66f6f79f601df349ac63
Jarrod Pan <jarrod.pan@gmail.com>
2025-12-12 17:07:32 +1100

    feat: generate 2025/08

---
+++ 2025/08/index.ts
@@ -0,0 +1,14 @@
+import fs from 'fs';
+
+console.log('Advent of Code 2025/08');
+const input = fs.readFileSync('./2025/08/input.txt', 'utf-8').split('\n');
+
+let ans = 0;
+
+for (let i = 0; i < input.length; i++) {
+  const line = input[i];
+  
+  // const groups = line.match(/(?<aaaaa>\w+)(?<bbbbb>\d+)/)!.groups!;
+}
+
+console.log({ ans });
